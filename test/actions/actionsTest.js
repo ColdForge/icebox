e5ebe03e3a6cde1b1d0d2a9397f0ab4590d65eea
Colin Zarnegar <c.zarnegar@gmail.com>
2016-06-10 19:44:25 -0700

    [setup] sets up testing environment

---
+++ test/actions/actionsTest.js
@@ -0,0 +1,7 @@
+import { expect } from '../testHelper';
+import {  } from '../../src/constants/actions';
+import {  } from '../../src/actions';
+
+describe('actions' , () => {
+	
+});
--- test/components/appTest.js
+++ test/components/appTest.js
@@ -16,15 +16,4 @@ describe('App' , () => {
     expect(component).to.exist;
   });
 
-  it('shows a comment box', () => {
-  	expect(component.find('.comment-box')).to.exist;
-  });
-
-  it('shows a comment list', () => {
-    expect(component.find('.comment-list')).to.exist;
-  });
-
-  it('shows a favorite list', () => {
-    expect(component.find('.favorite-list')).to.exist;
-  });
 });
