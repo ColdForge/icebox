dc42ba0f39c2e7078006e64328913c005ca5adbe
Colin Zarnegar <c.zarnegar@gmail.com>
2016-06-11 14:35:02 -0700

    [pull] converts appDrawer into functional component

--- src/components/appDrawer.js
+++ src/components/appDrawer.js
@@ -2,37 +2,20 @@ import React, { Component } from 'react';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 
-class AppDrawer extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {open: props.drawerOpen};
-	}
-	
-	handleClose = () => {
-		this.setState({open: false});
-		this.props.updateDrawer();
-	}
-	
-	render(){
-		return (
-			<Drawer
-				className="app-drawer"
-				overlayClassName="app-drawer-overlay"
-				docked={false}
-				width={400}
-				open={this.state.open}
-				onRequestChange={
-					(open) => {
-						this.setState({open});
-						this.props.updateDrawer();
-					}
-				}
-			>
-				<MenuItem className="app-drawer-profile" onTouchTap={this.handleClose}>Profile</MenuItem>
-				<MenuItem className="app-drawer-recipes" onTouchTap={this.handleClose}>Recipes</MenuItem>
-			</Drawer>
-		);
-	}
-}
+const AppDrawer = ({ drawerOpen, updateDrawer }) => {
+  return (
+  	<Drawer
+  		className="app-drawer"
+  		overlayClassName="app-drawer-overlay"
+  		docked={false}
+  		width={400}
+  		open={drawerOpen}
+  		onRequestChange={(open) => updateDrawer()}
+  	>
+  		<MenuItem className="app-drawer-profile" onTouchTap={() => updateDrawer()}>Profile</MenuItem>
+  		<MenuItem className="app-drawer-recipes" onTouchTap={() => updateDrawer()}>Recipes</MenuItem>
+  	</Drawer>
+  );
+};
 
 export default AppDrawer;
