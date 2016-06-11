7bcb5456076622b29d0a3c37f377a244e6880154
Austin Sefton <austinsefton@Austins-MacBook-Pro.local>
2016-06-11 11:23:30 -0700

    [fix] fixes IceboxList to correctly pass down props to IceboxListItem

--- src/components/iceboxList.js
+++ src/components/iceboxList.js
@@ -7,7 +7,7 @@ class IceboxList extends Component {
 		return (
       <List>
         {this.props.contents.map(item => (
-          <IceboxListItem  />
+          <IceboxListItem  item={item}/>
         ))}
       </List>
 		);
