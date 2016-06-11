6c96b4e94470917b810233263be472b58cc15f17
Austin Sefton <austinsefton@Austins-MacBook-Pro.local>
2016-06-11 16:10:59 -0700

    [fix] edits list items to include unique key

--- src/components/iceboxListItem.js
+++ src/components/iceboxListItem.js
@@ -3,21 +3,23 @@ import { ListItem } from 'material-ui/List';
 import SvgIcon from 'material-ui/SvgIcon';
 import ICONS from '../styles/icons';
 
-const IceboxListItem = ({name, foodGroup, expiration}) => (
-  <ListItem
-    className="iceboxListItem"
-    primaryText={name}
-    leftIcon={
-      <SvgIcon className="food-group-icon">
-        <path className="icon" d={ICONS.Dairy.d} />
-      </SvgIcon>
-    }
-    children={
-      <div>
-        <p>{expiration}</p>
-      </div>
-    }
-  />
+const IceboxListItem = ({ name, foodGroup, expiration }) => (
+      <ListItem
+        key={name+foodGroup}
+        className="iceboxListItem"
+        primaryText={name}
+        leftIcon={
+          <SvgIcon className="food-group-icon">
+            <path className="icon" d={ICONS.Dairy.d} />
+          </SvgIcon>
+        }
+        children={
+          <div>
+            <p>{expiration}</p>
+          </div>
+        }
+      />
 );
 
 export default IceboxListItem;
+
