cfd22f800c6200ab81b7799cd218e054fd83bace
Colin Zarnegar <c.zarnegar@gmail.com>
2016-06-11 12:52:36 -0700

    [pull] adds renderButtons method to appHeader component

--- src/components/appHeader.js
+++ src/components/appHeader.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import AppBar from 'material-ui/AppBar';
+import FlatButton from 'material-ui/FlatButton';
 import { connect } from 'react-redux';
 import * as actions from '../actions/index';
 
 class AppHeader extends Component {
+	renderButtons(){
+		// if a user is currently authenticated
+		if(this.props.auth){
+			// render the help and signout buttons
+			return (
+				<div>
+					<FlatButton label="Help" />
+					<FlatButton label="Signout" />
+				</div>
+			);
+		} else { // else
+			// render the signup and signin buttons
+			return (
+				<div>
+					<FlatButton label="Signup" />
+					<FlatButton label="Signin" />
+				</div>
+			);
+		}
+	}
 	render(){
 		return (
-			<div>This is the AppHeader component</div>
+			<AppBar
+				title="Icebox"
+				children={this.renderButtons()}
+			/>
 		);
 	}
 }
