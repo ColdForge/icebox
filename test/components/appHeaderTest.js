8c29f6b62d14937d722ab18da4b0690dc64965b2
Colin Zarnegar <c.zarnegar@gmail.com>
2016-06-11 12:50:40 -0700

    [pull] creates test framework for appHeader component

--- test/components/appHeaderTest.js
+++ test/components/appHeaderTest.js
@@ -12,6 +12,42 @@ describe('AppHeader' , () => {
     expect(component).to.exist;
   });
 
+  it('should have the title of "Icebox"', () => {
+  	expect(component).to.contain('Icebox');
+  });
 
+  describe('AppHeader interactivity', () => {
+  	xit('should show the side drawer when the MenuIconButton is clicked', () => {
 
+  	});
+
+  	describe('when no user signed in', () => {
+  		beforeEach(() => {
+  			component = renderComponent(AppHeader, null, { auth: false });
+  		});
+
+  		it('should have a signup button', () => {
+
+  		});
+
+  		it('should have a signin button', () => {
+  			
+  		});
+  	});
+
+  	describe('when a user is signed in', () => {
+  		beforeEach(() => {
+  			component = renderComponent(AppHeader, null, { auth: true });
+  		});
+
+  		it('should have a signup button', () => {
+
+  		});
+
+  		it('should have a signin button', () => {
+  			
+  		});
+  	});
+  });
+  
 });
