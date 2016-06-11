6748135f75ed41b56b82337921a30134bc4e78dd
Colin Zarnegar <c.zarnegar@gmail.com>
2016-06-11 16:42:09 -0700

    [pull] builds out initial toolbar with search, speech, and filter icon buttons

--- src/components/iceboxToolbar.js
+++ src/components/iceboxToolbar.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
 import { Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle } from 'material-ui/Toolbar';
+import IconButton from 'material-ui/IconButton';
+import SvgIcon from 'material-ui/SvgIcon';
+import ICONS from '../styles/icons';
+
+const styles = {
+	button: {
+		zIndex: 100
+	}
+}
 
 class IceboxToolbar extends Component {
 	render() {
 		return (
 			<Toolbar>
 				<ToolbarGroup>
-					<h4>IceboxToolbar</h4>
+					<IconButton tooltip="Search" style={styles.button}>
+						<SvgIcon>
+							<path d={ICONS.Search.d} />
+						</SvgIcon>
+					</IconButton>
+				</ToolbarGroup>
+				<ToolbarGroup>
+					<IconButton tooltip="Speech" style={styles.button}>
+						<SvgIcon>
+							<path d={ICONS.Speech.d} />
+						</SvgIcon>
+					</IconButton>
+				</ToolbarGroup>
+				<ToolbarGroup>
+					<IconButton tooltip="Filter" style={styles.button}>
+						<SvgIcon>
+							<path d={ICONS.Filter.d} />
+						</SvgIcon>
+					</IconButton>
 				</ToolbarGroup>
 			</Toolbar>
 		);
