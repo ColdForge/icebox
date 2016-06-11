eab2399977ff37aecaa2ba51ee174fcf7ae82010
Colin Zarnegar <c.zarnegar@gmail.com>
2016-06-11 13:01:41 -0700

    [pull] creates authReducer and connects it to combineReducers in reducers/index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from 'redux';
+import authReducer from './authReducer';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  auth: authReducer
 });
 
 export default rootReducer;
