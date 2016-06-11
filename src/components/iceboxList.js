c0149e203d38ae4cb344f539f026b5bf006d952e
Austin Sefton <austinsefton@Austins-MacBook-Pro.local>
2016-06-11 16:09:17 -0700

    [pull] adds dummy data and a unique key for items being rendered

--- src/components/iceboxList.js
+++ src/components/iceboxList.js
@@ -2,13 +2,25 @@ import React, { Component } from 'react';
 import { List } from 'material-ui/List';
 import IceboxListItem from './iceboxListItem';
 
+const CONTENTS = [
+    {name: 'Bananas', foodGroup: 'Produce', expiration: 5, id: 1},
+    {name: 'Ground Beef', foodGroup: 'Meat', expiration: 6, id: 2},
+    {name: 'Milk', foodGroup: 'Dairy', expiration: 1, id: 3}
+];
+
 class IceboxList extends Component {
 	render(){
 		return (
       <List>
-        {this.props.contents.map(item => (
-          <IceboxListItem item={item}/>
-        ))}
+        {CONTENTS.map(item => {
+          return (
+            <IceboxListItem
+              key={item.id}
+              item={item}
+            />
+          );
+
+        })}
       </List>
 		);
 	}
