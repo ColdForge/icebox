1442f360eabddb289a3d67761f71d64b818c55ec
Colin Zarnegar <c.zarnegar@gmail.com>
2016-06-11 15:53:09 -0700

    [pull] builds out initial tests for authReducer

--- test/reducers/reducersTest.js
+++ test/reducers/reducersTest.js
@@ -1,6 +1,26 @@
 import { expect } from '../testHelper';
-import {  } from '../../src/constants/actions';
+import * as TYPES from '../../src/constants/actions';
+import authReducer from '../../src/reducers/authReducer';
 
 describe('reducers' , () => {
-	
+	describe('authReducer', () => {
+		it('handles an action of unknown type', () => {
+		  expect(authReducer(undefined,{})).to.eql({});
+		});
+
+		it('correctly handles an action of type AUTHORIZE_USER', () => {
+			const action = {type:TYPES.AUTHORIZE_USER};
+			expect(authReducer(undefined,action)).to.eql({ error: '', authenticated: true });
+		});
+
+		it('correctly handles an action of type DEAUTHORIZE_USER', () => {
+			const action = {type:TYPES.DEAUTHORIZE_USER};
+			expect(authReducer(undefined,action)).to.eql({ authenticated: false });
+		});
+
+		it('correctly handles an action of type AUTHORIZE_ERROR', () => {
+			const action = {type:TYPES.AUTHORIZE_ERROR,payload:'Error message'};
+			expect(authReducer(undefined,action)).to.eql({ error: 'Error message' });
+		});
+	})
 });
