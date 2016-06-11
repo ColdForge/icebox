2ce5da60258c78f05a161e9508eeee7b45473401
Colin Zarnegar <c.zarnegar@gmail.com>
2016-06-11 16:27:41 -0700

    [setup] adds iceboxToolbar to Icebox container

--- src/containers/icebox.js
+++ src/containers/icebox.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../actions/index';
+import IceboxToolbar from '../components/iceboxToolbar';
 import IceboxList from '../components/iceboxList';
 
 class Icebox extends Component {
 	render(){
 		return (
 			<div>
+				<IceboxToolbar />
         <IceboxList />
       </div>
 		);
