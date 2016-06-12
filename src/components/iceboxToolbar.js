27046d6ff6a4058f7c2de7ec84212fa8634fbb38
Colin Zarnegar <c.zarnegar@gmail.com>
2016-06-11 23:30:22 -0700

    [refactor] refactors code in iceboxToolbar for legibility

--- src/components/iceboxToolbar.js
+++ src/components/iceboxToolbar.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle } from 'material-ui/Toolbar';
 import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
 import SvgIcon from 'material-ui/SvgIcon';
 import ICONS from '../styles/icons';
 
@@ -11,29 +13,79 @@ const styles = {
 }
 
 class IceboxToolbar extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			filterValue: "expiration",
+			searchTerm: ''
+		}
+	}
+
+	handleSearch(event, value) {
+
+	}
+
+	handleFilterChange(event, value) {
+		this.setState({ filterValue: value });
+	}
+
 	render() {
 		return (
 			<Toolbar>
 				<ToolbarGroup>
-					<IconButton tooltip="Search" style={styles.button}>
-						<SvgIcon>
+					<IconButton 
+						tooltip="Search" 
+						style={styles.button}
+						className="icebox-toolbar-search"
+
+					>
+						<SvgIcon className="icebox-toolbar-svgicon-search">
 							<path d={ICONS.Search.d} />
 						</SvgIcon>
 					</IconButton>
 				</ToolbarGroup>
 				<ToolbarGroup>
-					<IconButton tooltip="Speech" style={styles.button}>
-						<SvgIcon>
+					<IconButton 
+						tooltip="Speech" 
+						style={styles.button}
+						className="icebox-toolbar-speech"
+
+					>
+						<SvgIcon className="icebox-toolbar-svgicon-speech">
 							<path d={ICONS.Speech.d} />
 						</SvgIcon>
 					</IconButton>
 				</ToolbarGroup>
 				<ToolbarGroup>
-					<IconButton tooltip="Filter" style={styles.button}>
-						<SvgIcon>
-							<path d={ICONS.Filter.d} />
-						</SvgIcon>
-					</IconButton>
+					<IconMenu
+						iconButtonElement={
+							<IconButton 
+								tooltip="Sort" 
+								style={styles.button}
+								className="icebox-toolbar-sort"
+							>
+								<SvgIcon className="icebox-toolbar-svgicon-sort">
+									<path d={ICONS.Sort.d} />
+								</SvgIcon>
+							</IconButton>
+						}
+						anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+	      		targetOrigin={{horizontal: 'right', vertical: 'top'}}
+	      		value={this.state.filterValue}
+	      		onChange={this.handleFilterChange}
+					>
+						<MenuItem 
+							value="expiration"
+							primaryText="Sort By: Expiration"/>
+						<MenuItem
+							value="group" 
+							primaryText="Sort By: Food Group"/>
+						<MenuItem 
+							value="name"
+							primaryText="Sort By: Food Name"/>
+					</IconMenu>
+
+					
 				</ToolbarGroup>
 			</Toolbar>
 		);
