b8f0036d66ee87d8bc3d40bcf16532960271500b
Colin Zarnegar <c.zarnegar@gmail.com>
2016-06-13 10:50:14 -0700

    [pull] adds insertUser and hashPassword methods to userController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,9 +1,28 @@
-var db = require('../db/config').knex;
+var knex = require('../db/config').knex;
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 var _ = require('lodash');
 
 module.exports = {
+	insertUser: function(user) {
+		var pHash = Promise.promisify(hashPassword);
+		return pHash(user);
+	},
+	hashPassword: function(user) {
+		var cipher = Promise.promisify(bcrypt.hash);
+		return cipher(user.password, null, null)
+			.then(function(hash) {
+				knex('users')
+					.insert({
+						email: user.email,
+						name: user.name,
+						password: hash
+					})
+					.then(function(response){
+						console.log('inside hashPassword insert, response is : ',response);
+					})
+			})
+	},
 	comparePassword: function(testPassword, user, callback) {
 		console.log('comparePassword called in userController with testPassword: ',testPassword,' and user: ',user);
 		var cipher = Promise.promisify(bcrypt.compare);
