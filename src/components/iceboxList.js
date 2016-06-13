9f0d6e9a70b54a4093b59c5e76b2f78427a99eca
Austin Sefton <austinsefton@Austins-MacBook-Pro.local>
2016-06-13 16:58:16 -0700

    [pull] adds subheader to iceboxList

--- src/components/iceboxList.js
+++ src/components/iceboxList.js
@@ -6,21 +6,22 @@ const keyGenerator = () => {
   return Math.floor(Math.random()*100*Math.random()*22*Math.random()*43*Math.random()*1010102);
 }
 const CONTENTS = [
-    {name: 'Bananas', foodGroup: 'Produce', expiration: 5, key: String(keyGenerator())},
-    {name: 'Ground Beef', foodGroup: 'Meat', expiration: 6, key: String(keyGenerator())},
-    {name: 'Milk', foodGroup: 'Dairy', expiration: 1, key: String(keyGenerator())}
+    {name: 'Bananas', foodGroup: 'Produce', expiration: 5, key: keyGenerator()},
+    {name: 'Ground Beef', foodGroup: 'Meat', expiration: 6, key: keyGenerator()},
+    {name: 'Milk', foodGroup: 'Dairy', expiration: 1, key: keyGenerator()}
 ];
 
 class IceboxList extends Component {
 	render(){
 		return (
-      <List>
+      <List subheader="Grocery List Items">
         {CONTENTS.map(item => {
           return (
             <IceboxListItem
               name={item.name}
               foodGroup={item.foodGroup}
               expiration={item.expiration}
+              key={item.key}
             />
           );
         })}
