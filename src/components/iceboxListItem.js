5d1271f43277c95883ba530a589272bdd6dfb09a
Austin Sefton <austinsefton@Austins-MacBook-Pro.local>
2016-06-13 11:03:44 -0700

    [fix] passes down props successfully to iceboxListItem.js

--- src/components/iceboxListItem.js
+++ src/components/iceboxListItem.js
@@ -3,23 +3,38 @@ import { ListItem } from 'material-ui/List';
 import SvgIcon from 'material-ui/SvgIcon';
 import ICONS from '../styles/icons';
 
-const IceboxListItem = ({ name, foodGroup, expiration }) => (
-  <ListItem
-    key={name+foodGroup}
-    className="iceboxListItem"
-    primaryText={name}
-    leftIcon={
-      <SvgIcon className="food-group-icon">
-        <path className="icon" d={ICONS.Dairy.d} />
-       </SvgIcon>
-     }
-    children={
-      <div>
-        <p>{expiration}</p>
-      </div>
+const styles = {
+  ListItem: {
+    text: {
+      color: "#000000",
     }
-  />
-);
+  }
+}
+
+const IceboxListItem = ({ name, foodGroup, expiration }) => {
+  if (!name) {
+    // return <ListItem Loading...;
+  }
+  // const { name, foodGroup, expiration } = item;
+  return (
+    <ListItem
+
+      className="iceboxListItem"
+      primaryText={name}
+      leftIcon={
+        <SvgIcon className="food-group-icon">
+          <path className="icon" d={ICONS.Dairy.d} />
+         </SvgIcon>
+       }
+      children={
+        <div>
+          <p>{expiration}</p>
+        </div>
+      }
+      style={styles.ListItem.text}
+    />
+  );
+};
 
 export default IceboxListItem;
 
