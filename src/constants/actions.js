d00c81b458e09ff34baabe4d59ae4723c19c4038
Colin Zarnegar <c.zarnegar@gmail.com>
2016-06-14 11:10:12 -0700

    [pull] adds constants for sortBy reducer/actions

--- src/constants/actions.js
+++ src/constants/actions.js
@@ -2,3 +2,5 @@
 export const AUTHORIZE_USER = "AUTHORIZE_USER";
 export const DEAUTHORIZE_USER = "DEAUTHORIZE_USER";
 export const AUTHORIZE_ERROR = "AUTHORIZE_ERROR";
+// ActionTypes for handling sort
+export const SET_SORT = "SET_SORT";
