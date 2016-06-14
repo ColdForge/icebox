ffe096ba8d89f04b3ab1a992338ad2157eb68344
Colin Zarnegar <c.zarnegar@gmail.com>
2016-06-14 11:37:42 -0700

    [pull] adds sortBy and sortOrder reducers to combineReducers function

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,14 @@
 import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
 import authReducer from './authReducer';
+import sortByReducer from './sortByReducer';
+import sortOrderReducer from './sortOrderReducer';
 
 const rootReducer = combineReducers({
 	form,
   auth: authReducer,
+  sortBy: sortByReducer,
+  sortOrder: sortOrderReducer,
 });
 
 export default rootReducer;
