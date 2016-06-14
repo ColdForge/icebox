f30d7bcdf8557a88bc67a7685a66cc5a34fd6a8d
Colin Zarnegar <c.zarnegar@gmail.com>
2016-06-13 19:28:25 -0700

    [fix] fixes signin and signup functions in authController

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,7 @@
 // All authentication functionality in here
 var jwt = require('jwt-simple');
 var userController = require('./userController');
-var db = require('../db/config').knex;
+var knex = require('../db/config').knex;
 // import JWT secret from here
 var EnvConfig = require('../db/envConfig');
 
@@ -9,7 +9,8 @@ module.exports = {
 	// Signin function:
 	// req passed in has user attribute set to false if signin info was wrong or user does not exist, or a user object with all its attributes
 	signin: function(req, res, next) {
-		res.send({ token: tokenForUser(req.user), name: req.user.attributes.name, email: req.user.attributes.email });
+		console.log('req is : ',req);
+		res.send({ token: tokenForUser(req.user), id: req.user.id, name: req.user.name, email: req.user.email });
 	},
 
 	// Signup function
@@ -17,7 +18,8 @@ module.exports = {
 		var email = req.body.email;
 		var name = req.body.name;
 		var password = req.body.password;
-		var profilePic = req.body.profile_url;
+
+		var user = { email: email, name: name, password: password };
 
 		if(!email || !password) {
 			return res.status(422).send({ error: 'You must provide email and password' });
@@ -31,11 +33,11 @@ module.exports = {
 					return res.status(422).send({ error: 'Email is in use' });
 				}
 				console.log('response is : ',response);
-				userController.insertUser(response[0])
+				userController.insertUser(user)
 					.then(function(response){
 						console.log('New user created in authController.signup!');
 						console.log('response is : ',response);
-						res.json({ token: tokenForUser(user) });
+						res.json({ token: tokenForUser(response) });
 					})
 			})
 	}	
