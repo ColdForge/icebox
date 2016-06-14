f903d8c48a45ca58d81a6f2805cfdfba02edd496
yaoandrew <yaoandrew@yahoo.com>
2016-06-13 20:02:40 -0700

    [update]adds function to get recipes from API

---
+++ server/config/apiutils.js
@@ -0,0 +1,30 @@
+var request = require ('request');
+var key = require ('./apikeys');
+var QUERY = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/" +
+  "recipes/findByIngredients?fillIngredients=true&ingredients="
+var QUERY_TAIL = "&limitLicense=false&number=5&ranking=1"
+
+
+module.exports = {
+
+  getRecipeFromIngredients : function (fooditems) {
+    var ingredQueryString = fooditems.join('%2C');
+    var options = {
+      url : QUERY+ingredQueryString+QUERY_TAIL,
+      headers: {
+        "X-Mashape-Key" : key.RECIPE_KEY,
+        "Accept" : "application/json"
+      }
+    }
+
+    function callback(error, response, body) {
+      if (!error && response.statusCode == 200) {
+    var info = JSON.parse(body);
+    console.log ("Recipe request result ", info)
+    }
+
+  }
+
+  request (options, callback);
+  }
+}
