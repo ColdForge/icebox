872b8b369035885023f40d9cda2744915c081d0b
Colin Zarnegar <c.zarnegar@gmail.com>
2016-06-14 13:10:29 -0700

    [pull] refactors iceboxList to render props passed in, instead of dummy data

--- src/components/iceboxList.js
+++ src/components/iceboxList.js
@@ -3,15 +3,12 @@ import { List } from 'material-ui/List';
 import IceboxListItem from './iceboxListItem';
 import dataList from '../data/dummyFoodList';
 
-const keyGenerator = () => {
-  return Math.floor(Math.random()*100*Math.random()*22*Math.random()*43*Math.random()*1010102);
-}
-
 class IceboxList extends Component {
 	render(){
+    console.log('this.props is : ',this.props);
 		return (
       <List subheader="Grocery List Items">
-        {dataList.map(item => {
+        {this.props.contents.map(item => {
           return (
             <IceboxListItem
               name={item.name}
