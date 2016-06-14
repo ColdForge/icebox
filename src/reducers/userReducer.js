467d280636aae00053f0ebebe3590927dfd471a5
Colin Zarnegar <c.zarnegar@gmail.com>
2016-06-14 15:45:42 -0700

    [pull] creates userReducer and wires into combineReducers function

---
+++ src/reducers/userReducer.js
@@ -0,0 +1,11 @@
+import { GET_USER_INFO, UPDATE_USER_INFO } from '../constants/actions';
+
+export default function(state = {}, action) {
+	switch(action.type){
+		case GET_USER_INFO:
+			return {...state, id: action.payload.id, name: action.payload.name, email: action.payload.email, iceboxID: action.payload.iceboxID };
+		case UPDATE_USER_INFO:
+			return {...state, id: action.payload.id, name: action.payload.name, email: action.payload.email, iceboxID: action.payload.iceboxID };
+	}
+	return state;
+}
