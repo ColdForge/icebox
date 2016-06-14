7a3f21ac30ab43b0a9daaec0540cc8e76bf39733
Colin Zarnegar <c.zarnegar@gmail.com>
2016-06-14 12:38:24 -0700

    [pull] fixes iceboxReducer and adds it to combineReducers function

--- src/reducers/iceboxReducer.js
+++ src/reducers/iceboxReducer.js
@@ -2,12 +2,13 @@ import { POPULATE_ICEBOX, ADD_ITEMS, REMOVE_ITEMS } from '../constants/actions';
 import { v4 } from 'node-uuid';
 
 export default function(state = [], action) {
+	let newItems;
 	switch(action.type){
 		case POPULATE_ICEBOX:
-			let newItems = action.payload.contents.map(item => {...item, key: v4()});
+			newItems = action.payload.contents.map(item => ({...item, key: v4()}));
 			return [...state, ...newItems];
 		case ADD_ITEMS:
-			let newItems = action.payload.contents.map(item => {...item, key: v4()});
+			newItems = action.payload.contents.map(item => ({...item, key: v4()}));
 			return [...state, ...newItems];
 		case REMOVE_ITEMS:
 			
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,14 @@ import { reducer as form } from 'redux-form';
 import authReducer from './authReducer';
 import sortByReducer from './sortByReducer';
 import sortOrderReducer from './sortOrderReducer';
+import iceboxReducer from './iceboxReducer';
 
 const rootReducer = combineReducers({
 	form,
   auth: authReducer,
   sortBy: sortByReducer,
   sortOrder: sortOrderReducer,
+  icebox: iceboxReducer,
 });
 
 export default rootReducer;
