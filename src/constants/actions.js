4e9b93f9b14d4c86743cf6aaf7f975f3143caafc
Colin Zarnegar <c.zarnegar@gmail.com>
2016-06-14 11:23:35 -0700

    [pull] creates constants for sortOrder

--- src/constants/actions.js
+++ src/constants/actions.js
@@ -4,3 +4,4 @@ export const DEAUTHORIZE_USER = "DEAUTHORIZE_USER";
 export const AUTHORIZE_ERROR = "AUTHORIZE_ERROR";
 // ActionTypes for handling sort
 export const SET_SORT = "SET_SORT";
+export const SET_SORT_ORDER = "SET_SORTORDER";
