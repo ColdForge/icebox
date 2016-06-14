bd1f5472597a40075e01edc732a9ba214b8f3e67
Colin Zarnegar <c.zarnegar@gmail.com>
2016-06-14 15:56:16 -0700

    [pull] adds more items to dummyFoodList

--- src/data/dummyFoodList.js
+++ src/data/dummyFoodList.js
@@ -9,7 +9,22 @@ const dataList = [
     {name: 'Munster Cheese', foodGroup: 'Dairy', expiration: 11, iconPath: '../../assets/dairy.png', id: 8},
     {name: 'Apples', foodGroup: 'Fruits', expiration: 5, iconPath: '../../assets/fruit.png', id: 9},
     {name: 'Oranges', foodGroup: 'Fruits', expiration: 6, iconPath: '../../assets/fruit.png', id: 10},
-    {name: 'Eggs', foodGroup: 'Dairy', expiration: 10, iconPath: '../../assets/meat.png', id: 11}
+    {name: 'Venison', foodGroup: 'Meat', expiration: 10, iconPath: '../../assets/meat.png', id: 11},
+    {name: 'Tuna', foodGroup: 'Meat', expiration: 10, iconPath: '../../assets/meat.png', id: 12},
+    {name: 'Pork chops', foodGroup: 'Meat', expiration: 13, iconPath: '../../assets/meat.png', id: 13},
+    {name: 'Ribeye', foodGroup: 'Meat', expiration: 13, iconPath: '../../assets/meat.png', id: 14},
+    {name: 'Yogurt', foodGroup: 'Dairy', expiration: 13, iconPath: '../../assets/dairy.png', id: 15},
+    {name: 'Mascarpone Cheese', foodGroup: 'Dairy', expiration: 4, iconPath: '../../assets/dairy.png', id: 16},
+    {name: 'Eggplant', foodGroup: 'Vegetables', expiration: 3, iconPath: '../../assets/vegetables.png', id: 17},
+    {name: 'Radishes', foodGroup: 'Vegetables', expiration: 3, iconPath: '../../assets/vegetables.png', id: 18},
+    {name: 'Zucchini', foodGroup: 'Vegetables', expiration: 4, iconPath: '../../assets/vegetables.png', id: 19},
+    {name: 'Squash', foodGroup: 'Vegetables', expiration: 7, iconPath: '../../assets/vegetables.png', id: 20},
+    {name: 'Cherries', foodGroup: 'Fruit', expiration: 7, iconPath: '../../assets/fruit.png', id: 21},
+    {name: 'Plums', foodGroup: 'Fruit', expiration: 8, iconPath: '../../assets/fruit.png', id: 22},
+    {name: 'Peaches', foodGroup: 'Fruit', expiration: 8, iconPath: '../../assets/fruit.png', id: 23},
+    {name: 'Pineapple', foodGroup: 'Fruit', expiration: 17, iconPath: '../../assets/fruit.png', id: 24},
+    {name: 'Dates', foodGroup: 'Fruit', expiration: 17, iconPath: '../../assets/fruit.png', id: 25}
+
 ];
 
 export default dataList;
