c3902d2a2ffa3621dde72cbe4fcf3c55f41bb3a8
Sefton419 <austin.sefton@me.com>
2016-06-15 12:11:51 -0700

    [pull] adds more items to dummy data to display grains and snacks icon

--- src/data/dummyFoodList.js
+++ src/data/dummyFoodList.js
@@ -1,30 +1,34 @@
 const dataList = [
-    {name: 'Bananas', foodGroup: 'Produce', expiration: 5, iconPath: '../../assets/fruit.png', id: 1},
+    {name: 'Bananas', foodGroup: 'Fruit', expiration: 5, iconPath: '../../assets/fruit.png', id: 1},
     {name: 'Ground Beef', foodGroup: 'Meat', expiration: 6, iconPath: '../../assets/meat.png', id: 2},
     {name: 'Milk', foodGroup: 'Dairy', expiration: 1, iconPath: '../../assets/dairy.png', id: 3},
     {name: 'Lettuce', foodGroup: 'Vegetables', expiration: 1, iconPath: '../../assets/vegetables.png', id: 4},
-    {name: 'Tomatoes', foodGroup: 'Vegetables', expiration: 3, iconPath: '../../assets/fruit.png', id: 5},
+    {name: 'Tomatoes', foodGroup: 'Vegetables', expiration: 3, iconPath: '../../assets/vegetables.png', id: 5},
     {name: 'Potatoes', foodGroup: 'Vegetables', expiration: 21, iconPath: '../../assets/vegetables.png', id: 6},
+    {name: 'English Muffins', foodGroup: 'Grains', expiration: 9, iconPath: '../../assets/grains.png', id: 26},
     {name: 'Chicken Breast', foodGroup: 'Meat', expiration: 4, iconPath: '../../assets/meat.png', id: 7},
     {name: 'Munster Cheese', foodGroup: 'Dairy', expiration: 11, iconPath: '../../assets/dairy.png', id: 8},
     {name: 'Apples', foodGroup: 'Fruits', expiration: 5, iconPath: '../../assets/fruit.png', id: 9},
+    {name: 'Popcorn', foodGroup: 'Snacks', expiration: 30, iconPath: '../../assets/snacks.png', id: 18},
     {name: 'Oranges', foodGroup: 'Fruits', expiration: 6, iconPath: '../../assets/fruit.png', id: 10},
+    {name: 'Sourdough Bread', foodGroup: 'Grains', expiration: 3, iconPath: '../../assets/grains.png', id:27 },
     {name: 'Venison', foodGroup: 'Meat', expiration: 10, iconPath: '../../assets/meat.png', id: 11},
     {name: 'Tuna', foodGroup: 'Meat', expiration: 10, iconPath: '../../assets/meat.png', id: 12},
     {name: 'Pork chops', foodGroup: 'Meat', expiration: 13, iconPath: '../../assets/meat.png', id: 13},
     {name: 'Ribeye', foodGroup: 'Meat', expiration: 13, iconPath: '../../assets/meat.png', id: 14},
     {name: 'Yogurt', foodGroup: 'Dairy', expiration: 13, iconPath: '../../assets/dairy.png', id: 15},
+    {name: 'Pita Bread', foodGroup: 'Grains', expiration: 11, iconPath: '../../assets/grains.png', id: 28},
     {name: 'Mascarpone Cheese', foodGroup: 'Dairy', expiration: 4, iconPath: '../../assets/dairy.png', id: 16},
     {name: 'Eggplant', foodGroup: 'Vegetables', expiration: 3, iconPath: '../../assets/vegetables.png', id: 17},
     {name: 'Radishes', foodGroup: 'Vegetables', expiration: 3, iconPath: '../../assets/vegetables.png', id: 18},
     {name: 'Zucchini', foodGroup: 'Vegetables', expiration: 4, iconPath: '../../assets/vegetables.png', id: 19},
     {name: 'Squash', foodGroup: 'Vegetables', expiration: 7, iconPath: '../../assets/vegetables.png', id: 20},
     {name: 'Cherries', foodGroup: 'Fruit', expiration: 7, iconPath: '../../assets/fruit.png', id: 21},
+    {name: 'Cereal', foodGroup: 'Grains', expiration: 16, iconPath: '../../assets/grains.png', id: 25},
     {name: 'Plums', foodGroup: 'Fruit', expiration: 8, iconPath: '../../assets/fruit.png', id: 22},
     {name: 'Peaches', foodGroup: 'Fruit', expiration: 8, iconPath: '../../assets/fruit.png', id: 23},
     {name: 'Pineapple', foodGroup: 'Fruit', expiration: 17, iconPath: '../../assets/fruit.png', id: 24},
-    {name: 'Dates', foodGroup: 'Fruit', expiration: 17, iconPath: '../../assets/fruit.png', id: 25}
-
+    {name: 'Dates', foodGroup: 'Fruit', expiration: 17, iconPath: '../../assets/fruit.png', id: 29}
 ];
 
 export default dataList;
