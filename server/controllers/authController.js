c643601989301be302462926729dd95f3ed30037
Colin Zarnegar <c.zarnegar@gmail.com>
2016-06-15 16:37:40 -0700

    [pull] adds functionality for authController.signin to return icebox contents

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -9,8 +9,21 @@ module.exports = {
 	// Signin function:
 	// req passed in has user attribute set to false if signin info was wrong or user does not exist, or a user object with all its attributes
 	signin: function(req, res, next) {
-			res.send({token: tokenForUser(req.user), id: req.user.id, name: req.user.name, email: req.user.email, iceboxID: req.user.iceboxID});
-		},
+		knex('icebox_items')
+			.join('foods', 'icebox_items.foodID', '=', 'foods.id')
+  		.select('icebox_items.daysToExpire as expiration', 'foods.category as foodGroup', 'foods.name as name', 'icebox_items.foodID as foodID')
+  		.where('icebox_items.iceboxID',req.user.iceboxID)
+  		.then(function(response){
+  			res.send({
+  				token: tokenForUser(req.user), 
+  				id: req.user.id, 
+  				name: req.user.name, 
+  				email: req.user.email, 
+  				iceboxID: req.user.iceboxID, 
+  				contents: response 
+  			});
+  		})
+	},
 
 		// Signup function
 	signup: function(req, res, next) {
@@ -29,12 +42,10 @@ module.exports = {
 		  .select('*')
 		  .where('email',email)
 		  .then(function(response){
-		    console.log('response inside knex select statement: ',response);
 		    if(response.length > 0){
 		      return res.status(422).send({ error: 'Email is in use' });
 		    }
 		    else {
-		      console.log('response is : ',response);
 		        knex.insert({user_email: user.email})
 		          .into('iceboxes')
 		          .then(function(resp){
@@ -47,10 +58,8 @@ module.exports = {
 		                  password: hash,
 		                  iceboxID: user.iceboxID
 		                }).then(function(response){
-		                  console.log('user is', user);
-		                  console.log('response is', response);
-		                  var userObj = Object.assign({ id: response }, user );
-		                  res.json({ token: tokenForUser(userObj), id: response, name: user.name, email: user.email, iceboxID: user.iceboxID });
+		                  var userObj = Object.assign({ id: response[0] }, user );
+		                  res.json({ token: tokenForUser(userObj), id: response[0], name: user.name, email: user.email, iceboxID: user.iceboxID });
 		                });
 		            })
 		        });
@@ -62,8 +71,6 @@ module.exports = {
 
 // Helper function to create token for user
 function tokenForUser(user) {
-	console.log('tokenForUser fired');
-	console.log('user id in tokenForUser is : ',user.id);
 	var timestamp = new Date().getTime();
 	return jwt.encode({ sub: user.id, iat: timestamp }, EnvConfig.SECRET);
 }
