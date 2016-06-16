74aed7a47025335428811e19d86fdc938e3c0ac0
Colin Zarnegar <c.zarnegar@gmail.com>
2016-06-16 15:35:24 -0700

    [refactor] refactors actions/index and App component to follow AirBnB style

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import AppHeader from './appHeader';
 
 const styles = {
@@ -14,30 +14,51 @@ const styles = {
 	},
 	body: {
 		flex: 10,
-
 	},
-}
+};
 
-class App extends Component {
-  render() {
-    return (
-      <div style={styles.container}>
-      	<div style={styles.header}>
-      		<AppHeader />
-      	</div>
-      	<div style={styles.body} className="app-body">
-      		<ReactCSSTransitionGroup
-		        component="div" transitionName="example" className="animation-container"
-		        transitionEnterTimeout={500} transitionLeaveTimeout={500}
-		      >
-		      	{React.cloneElement(this.props.children, {
-    	        key: location.pathname
-    	      })}
-      		</ReactCSSTransitionGroup>
-      	</div>
-      </div>
-    );
-  }
-}
+export const App = ({ child }) => (
+	<div style={styles.container}>
+		<div style={styles.header}>
+			<AppHeader />
+		</div>
+		<div style={styles.body} className="app-body">
+			<ReactCSSTransitionGroup
+				component="div" transitionName="example" className="animation-container"
+				transitionEnterTimeout={500} transitionLeaveTimeout={500}
+			>
+				{React.cloneElement(child, {
+					key: location.pathname,
+				})}
+			</ReactCSSTransitionGroup>
+		</div>
+	</div>
+);
 
-export default App;
+App.propTypes = {
+	child: React.PropTypes.element.isRequired,
+};
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div style={styles.container}>
+//       	<div style={styles.header}>
+//       		<AppHeader />
+//       	</div>
+//       	<div style={styles.body} className="app-body">
+//       		<ReactCSSTransitionGroup
+// 		        component="div" transitionName="example" className="animation-container"
+// 		        transitionEnterTimeout={500} transitionLeaveTimeout={500}
+// 		      >
+// 		      	{React.cloneElement(this.props.children, {
+//     	        key: location.pathname
+//     	      })}
+//       		</ReactCSSTransitionGroup>
+//       	</div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
