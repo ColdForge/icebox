9bfef7b5a3f5456e962f934048f04735f74306a1
Colin Zarnegar <c.zarnegar@gmail.com>
2016-06-16 16:40:42 -0700

    [refactor] refactors Icebox component to follow style guide

--- src/components/icebox.js
+++ src/components/icebox.js
@@ -2,30 +2,9 @@ import React from 'react';
 import IceboxToolbar from '../containers/iceboxToolbar';
 import VisibleIceboxList from '../containers/visibleIceboxList';
 
-const Icebox = () => {
-	return (
-		<div className="icebox-container">
-			<IceboxToolbar />
-      <VisibleIceboxList />
-    </div>
-	);
-}
-
-export default Icebox;
-
-// class Icebox extends Component {
-// 	render(){
-// 		return (
-// 			<div className="icebox-container">
-// 				<IceboxToolbar />
-//         <VisibleIceboxList />
-//       </div>
-// 		);
-// 	}
-// }
-
-// function mapStateToProps(state){
-// 	return { icebox: state.icebox, user: state.user };
-// }
-
-// export default connect(mapStateToProps,actions)(Icebox);
+export const Icebox = () => (
+	<div className="icebox-container">
+		<IceboxToolbar />
+		<VisibleIceboxList />
+	</div>
+);
