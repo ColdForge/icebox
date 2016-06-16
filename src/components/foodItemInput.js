51e2770d53934c1e71a0afc72b2e454620c2b63b
Sefton419 <austin.sefton@me.com>
2016-06-16 15:30:39 -0700

    [pull] adds foodItemInput component to components folder with input tag

---
+++ src/components/foodItemInput.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import {Tabs, Tab} from 'material-ui/Tabs';
+
+const FoodItemInput = () => {
+  return (
+    <input size="100"/>
+  );
+}
+
+export default FoodItemInput
