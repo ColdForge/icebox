5f8a4c3bb74723e27ee014c587c61c6ca443f690
Sefton419 <austin.sefton@me.com>
2016-06-16 18:51:39 -0700

    [pull] adds speech recognition logic

--- src/components/foodItemInput.js
+++ src/components/foodItemInput.js
@@ -1,8 +1,50 @@
-import React from 'react';
-// import {Tabs, Tab} from 'material-ui/Tabs';
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import {Tabs, Tab} from 'material-ui/Tabs';
 
-const FoodItemInput = () => (
-	<input size="100" />
-);
+const grammar = '#JSGF V1.0; grammar foods; public <food> = milk | eggs | bread | tomatoes | potatoes | lettuce | kale | bananas | apples | oranges | turkey breast ;';
+
+class FoodItemInput extends Component {
+  constructor(props) {
+    super(props);
+
+    console.log('props: ', props);
+
+
+    this.state = {term: ''}
+
+  }
+
+  render() {
+    return (
+      <div className="speech-text-input">
+        <input
+          size="100"
+          value={this.state.term}
+          onChange={event => this.i}
+        />
+      </div>
+    );
+  }
+
+}
+
+  const createGrammarList = () => {
+
+    var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+    var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+    var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+
+    var recognition = new SpeechRecognition();
+    var speechRecognitionList = new SpeechGrammarList();
+    speechRecognitionList.addFromString(grammar, 1);
+    recognition.interimResults = true;
+
+    var speechFlag = false;
+    var speechResults = [];
+
+  }
+
+
 
 export default FoodItemInput;
--- src/components/iceboxList.js
+++ src/components/iceboxList.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import { List } from 'material-ui/List';
 import IceboxListItem from './iceboxListItem';
-// import FoodItemInput from './foodItemInput';
+import FoodItemInput from './foodItemInput';
 
 const IceboxList = ({ contents }) => (
-	<List className="icebox-list">
-		{contents.map(item => (
-			<IceboxListItem
-				key={item.key}
-				item={item}
-				name={item.name}
-				foodGroup={item.foodGroup}
-				expiration={item.expiration}
-				iconPath={item.iconPath}
-			/>
-		))}
-	</List>
+  <div>
+    <FoodItemInput contents={contents} />
+  	<List className="icebox-list">
+  		{contents.map(item => (
+  			<IceboxListItem
+  				key={item.key}
+  				item={item}
+  				name={item.name}
+  				foodGroup={item.foodGroup}
+  				expiration={item.expiration}
+  				iconPath={item.iconPath}
+  			/>
+  		))}
+  	</List>
+  </div>
 );
 
 IceboxList.propTypes = {
 	contents: React.PropTypes.array.isRequired,
 };
+
 
 export default IceboxList;
