a9c5d6589e5a852c8d4d448dd37cc1eaa0f513b5
yaoandrew <yaoandrew@yahoo.com>
2016-06-17 16:41:10 -0700

    [fix] fixes recipe component so page will render

---
+++ src/data/dummyRecipeList.js
@@ -0,0 +1,36 @@
+const dataList = [
+    {
+        name: 'Char-Grilled Beef Tenderloin with Three-Herb Chimichurri',
+        image: 'https://spoonacular.com/recipeImages/char-grilled-beef-tenderloin-with-three-herb-chimichurri-156992.jpg',
+        readyInMinutes: 45,
+        sourceUrl: 'http://www.epicurious.com/recipes/food/views/Char-Grilled-Beef-Tenderloin-with-Three-Herb-Chimichurri-235342',
+        recipeID: 156992,
+        key: "4kswGMuPMeV8Z8vZ"
+    },
+    {
+        name: 'Fried Bananas and sausage with eggplant',
+        image: 'https://lh4.ggpht.com/XbXpwfKONYemd2mxJF2-IgQ9rKjw0hk6dSljSWwy_MI_5QbK0tuU3ocfPowWZ_ED-g=w300',
+        readyInMinutes: 15,
+        sourceUrl: 'http://www.epicurious.com/recipes/food/views/Char-Grilled-Beef-Tenderloin-with-Three-Herb-Chimichurri-235342',
+        recipeID: 15694392,
+        key: "4kswGMuPMafdsaeV8Z8vZ"
+    },
+    {
+        name: 'Sausage, hot dog, cucumber salad',
+        image: 'https://www.organicfacts.net/wp-content/uploads/2013/06/Cucumber.jpg',
+        readyInMinutes: 45,
+        sourceUrl: 'http://www.epicurious.com/recipes/food/views/Char-Grilled-Beef-Tenderloin-with-Three-Herb-Chimichurri-235342',
+        recipeID: 15622992,
+        key: "4kswGMuPMeggggV8Z8vZ"
+    },
+    {
+        name: 'Hard boiled eggs and Bananas',
+        image: 'https://spoonacular.com/recipeImages/char-grilled-beef-tenderloin-with-three-herb-chimichurri-156992.jpg',
+        readyInMinutes: 435,
+        sourceUrl: 'http://www.epicurious.com/recipes/food/views/Char-Grilled-Beef-Tenderloin-with-Three-Herb-Chimichurri-235342',
+        recipeID: 15456992,
+        key: "4kswGMuPMewsfeV8Z8vZ"
+    },
+];
+
+export default dataList;
