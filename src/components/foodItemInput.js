209f10b41f9a4bc464f2d8b8888cf134c8480d95
Sefton419 <austin.sefton@me.com>
2016-06-17 10:45:31 -0700

    [refactor] passes style guide tests for jslint

--- src/components/foodItemInput.js
+++ src/components/foodItemInput.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 // import { connect } from 'react-redux';
 // import { Tabs, Tab } from 'material-ui/Tabs';
 
-const grammar = [
-	'#JSGF V1.0; grammar foods;',
-	' public <food> = milk | eggs |',
-	' bread | tomatoes | potatoes |',
-	' lettuce | kale | bananas | apples |',
-	' oranges | turkey breast ;',
-].join(',');
+// const grammar = [
+// 	'#JSGF V1.0; grammar foods;',
+// 	' public <food> = milk | eggs |',
+// 	' bread | tomatoes | potatoes |',
+// 	' lettuce | kale | bananas | apples |',
+// 	' oranges | turkey breast ;',
+// ].join(',');
 
 class FoodItemInput extends Component {
 	constructor(props) {
@@ -30,21 +30,20 @@ class FoodItemInput extends Component {
 	}
 }
 
-const createGrammarList = () => {
+// const createGrammarList = () => {
+// 	const SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+// 	const SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+// 	const SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
 
-	const SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
-	const SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
-	const SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+// 	const recognition = new SpeechRecognition();
+// 	const speechRecognitionList = new SpeechGrammarList();
+// 	speechRecognitionList.addFromString(grammar, 1);
+// 	recognition.interimResults = true;
 
-	const recognition = new SpeechRecognition();
-	const speechRecognitionList = new SpeechGrammarList();
-	speechRecognitionList.addFromString(grammar, 1);
-	recognition.interimResults = true;
+// 	let speechFlag = false;
+// 	let speechResults = [];
 
-	let speechFlag = false;
-	let speechResults = [];
-
-	return;
-};
+// 	return;
+// };
 
 export default FoodItemInput;
