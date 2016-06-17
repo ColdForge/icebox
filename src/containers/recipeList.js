8a10ef64079180cd1f5484eafdf4196eb7e3347b
Colin Zarnegar <c.zarnegar@gmail.com>
2016-06-17 12:30:28 -0700

    [pull] creates recipeList container and connects it to state

---
+++ src/containers/recipeList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { List } from 'material-ui/List';
+import RecipeListItem from '../components/recipeListItem';
+
+const IceboxList = ({ recipes }) => (
+	<div>
+		<List className="icebox-list">
+			{recipes.map(recipe => (
+				<RecipeListItem
+					key={recipe.key}
+					name={recipe.name}
+				/>
+			))}
+		</List>
+	</div>
+);
+
+IceboxList.propTypes = {
+	recipes: React.PropTypes.array.isRequired,
+};
+
+const mapStateToProps = state => ({
+	recipes: state.recipes.past,
+});
+
+export default connect(mapStateToProps)(IceboxList);
