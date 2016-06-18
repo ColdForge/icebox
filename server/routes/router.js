3d4e2d30c22dc1402013664d70c459a1cbc8eb2c
Colin Zarnegar <c.zarnegar@gmail.com>
2016-06-18 12:36:08 -0700

    [refactor] refactors server routes and helpers for readability

--- server/routes/router.js
+++ server/routes/router.js
@@ -2,17 +2,22 @@ var helpers = require('../config/helpers');
 var express = require('express'),
   router = express.Router();
 
+// Route when user gets all items in icebox
+router.get('/icebox', helpers.fetchIceboxContents)
+// Route when user adds items to icebox
+router.post('/icebox', helpers.changeIceboxContents)
+// Get specific information on one item in a user's icebox
+router.get('/icebox/:id', helpers.getIceboxItem)
+// Delete a specific item in a user's icebox
+router.delete('/icebox/:id', helpers.deleteIceboxItem)
 // Route when user gets past recipe choices
 router.get('/icebox/pastRecipes', helpers.getPreviousRecipes)
 // Route when user gets recipe suggestions
-router.get('/icebox/recipes', helpers.getRecipes)
+router.get('/icebox/recipes', helpers.getRecipeSuggestions)
 // Route when user selects a recipe suggestion
-router.post('/icebox/recipes', helpers.postRecipe)
+router.post('/icebox/recipes', helpers.chooseRecipeSuggestion)
+// Get recipe details for specific recipe
 router.get('/icebox/recipe_details', helpers.getRecipeDetails)
-router.get('/icebox/:id', helpers.getItem)
-router.delete('/icebox/:id', helpers.deleteItem)
-router.get('/icebox', helpers.getAllItems)
-router.post('/icebox', helpers.postAllItems)
 
 module.exports = router;
 
