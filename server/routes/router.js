b19810bd0c7e5db2ef3b11c4e34d124a47d90448
Colin Zarnegar <c.zarnegar@gmail.com>
2016-06-18 12:16:08 -0700

    [pull] corrects server router module to use correct middleware

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,21 +1,37 @@
 var helpers = require('../config/helpers');
+var express = require('express'),
+  router = express.Router();
 
-module.exports = function (app) {
+// Route when user gets past recipe choices
+router.get('/icebox/pastRecipes', helpers.getPreviousRecipes)
+// Route when user gets recipe suggestions
+router.get('/icebox/recipes', helpers.getRecipes)
+// Route when user selects a recipe suggestion
+router.post('/icebox/recipes', helpers.postRecipe)
+router.get('/icebox/recipe_details', helpers.getRecipeDetails)
+router.get('/icebox/:id', helpers.getItem)
+router.delete('/icebox/:id', helpers.deleteItem)
+router.get('/icebox', helpers.getAllItems)
+router.post('/icebox', helpers.postAllItems)
 
-    app.route('api/icebox/recipes')
-      .get(helpers.getRecipes)
-      .post(helpers.postRecipe);
+module.exports = router;
 
-    app.get('api/icebox/recipe_details', helpers.getRecipeDetails);
+// module.exports = function (app) {
 
-    app.get('api/icebox/previous_recipes', helpers.getPreviousRecipes);
+//     app.route('api/icebox/recipes')
+//       .get(helpers.getRecipes)
+//       .post(helpers.postRecipe);
 
-    app.route('api/icebox/:id')
-      .get(helpers.getItem)
-      .delete(helpers.deleteItem);
+//     app.get('api/icebox/recipe_details', helpers.getRecipeDetails);
 
-    app.route('api/icebox')
-      .get(helpers.getAllItems)
-      .post(helpers.postAllItems);
+//     app.get('api/icebox/previous_recipes', helpers.getPreviousRecipes);
 
-}
+//     app.route('api/icebox/:id')
+//       .get(helpers.getItem)
+//       .delete(helpers.deleteItem);
+
+//     app.route('api/icebox')
+//       .get(helpers.getAllItems)
+//       .post(helpers.postAllItems);
+
+// }
