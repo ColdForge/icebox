958f8cb97d63ac22da94b38d905747cc531471d9
Sefton419 <austin.sefton@me.com>
2016-06-20 17:19:32 -0700

    [pull] creates landing page elements and basic structure

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
-import { 
+import {
 	green500,
 	red500,
 	blue500,
