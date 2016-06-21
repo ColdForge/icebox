f7a229f2efe341eb55d549653a1cdfb4d43fa087
Colin Zarnegar <c.zarnegar@gmail.com>
2016-06-20 19:24:44 -0700

    [pull] adds apply foodgroupicon to icebox reducer

--- src/reducers/iceboxReducer.js
+++ src/reducers/iceboxReducer.js
@@ -1,6 +1,49 @@
 import { POPULATE_ICEBOX, ADD_ITEMS, REMOVE_ITEMS } from '../constants/actions';
 import { v4 } from 'node-uuid';
 
+const applyFoodGroupIcon = (item) => {
+	switch (item.foodGroup.toLowerCase()) {
+	case 'fruit':
+		return ({ ...item, iconPath: '../../assets/fruit.png' });
+	case 'vegetables':
+		return ({ ...item, iconPath: '../../assets/vegetables.png' });
+	case 'dairy':
+		return ({ ...item, iconPath: '../../assets/dairy.png' });
+	case 'meats':
+		return ({ ...item, iconPath: '../../assets/meats.png' });
+	case 'poultry':
+		return ({ ...item, iconPath: '../../assets/poultry.png' });
+	case 'pork':
+		return ({ ...item, iconPath: '../../assets/pork.png' });
+	case 'beef':
+		return ({ ...item, iconPath: '../../assets/beef.png' });
+	case 'seafood':
+		return ({ ...item, iconPath: '../../assets/seafood.png' });
+	case 'grains':
+		return ({ ...item, iconPath: '../../assets/grains.png' });
+	case 'sauces':
+		return ({ ...item, iconPath: '../../assets/sauces.png' });
+	case 'legumes':
+		return ({ ...item, iconPath: '../../assets/legumes.png' });
+	case 'sweets':
+		return ({ ...item, iconPath: '../../assets/sweets.png' });
+	case 'snacks':
+		return ({ ...item, iconPath: '../../assets/snacks.png' });
+	case 'oils':
+		return ({ ...item, iconPath: '../../assets/oils.png' });
+	case 'beverages':
+		return ({ ...item, iconPath: '../../assets/beverages.png' });
+	case 'lamb':
+		return ({ ...item, iconPath: '../../assets/lamb.png' });
+	case 'nuts':
+		return ({ ...item, iconPath: '../../assets/nuts.png' });
+	case 'sides':
+		return ({ ...item, iconPath: '../../assets/sides.png' });
+	default:
+		return ({ ...item, iconPath: '../../assets/snacks.png' });
+	}
+};
+
 export default function (state = [], action) {
 	let newItems;
 	switch (action.type) {
@@ -23,7 +66,7 @@ export default function (state = [], action) {
 		});
 		return [...state, ...newItems];
 	case ADD_ITEMS:
-		newItems = action.payload.map(item => ({ ...item, key: v4() }));
+		newItems = action.payload.map(item => ({ ...applyFoodGroupIcon(item), key: v4() }));
 		return [...state, ...newItems];
 	case REMOVE_ITEMS:
 		return state;
@@ -31,3 +74,4 @@ export default function (state = [], action) {
 		return state;
 	}
 }
+
