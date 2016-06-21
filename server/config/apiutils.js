6e12a5b9be0dfd23fc4bc7f51395ca136188431d
Colin Zarnegar <c.zarnegar@gmail.com>
2016-06-20 18:57:27 -0700

    [pull] builds out getFoodType and matchFoodGroup functions in apiutils module

--- server/config/apiutils.js
+++ server/config/apiutils.js
@@ -1,5 +1,6 @@
-var request = require ('request');
-var key = require ('./apikeys');
+// var request = require ('request');
+var request = require('request-promise');
+var API_KEY = require ('./apikeys');
 
 // API Query Strings RECQUERY and RECQUERY_TAIL get the suggested recipe list
 // based on a list of ingredients passed into the function
@@ -15,15 +16,27 @@ var RECDETAIL_QUERY = "https://spoonacular-recipe-food-nutrition-" +
 var FOODTYPE = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/"+
   "recipes/parseIngredients";
 
+const SPOONACULAR = {
+  BASE: "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/",
+  CLASSIFY: "food/products/classify",
+  CLASSIFY_BATCH: "food/products/classifyBatch",
+  FIND_BY_INGREDIENTS: "recipes/findByIngredients?fillIngredients=true&ingredients=",
+  FIND_BY_INGREDIENTS_TAIL: "&limitLicense=false&number=3&ranking=1",
+  RECIPE_INFORMATION: "recipes/",
+  RECIPE_INFORMATION_TAIL: "/information?includeNutrition=false"
+};
+
+const USDA = {
+  BASE: "http://api.nal.usda.gov/ndb/reports/"
+};
+
 module.exports = {
-
   getRecipeFromIngredients : function (fooditems, cb) {
-
     var ingredQueryString = fooditems.join('%2C');
     var options = {
       url : RECQUERY+ingredQueryString+RECQUERY_TAIL,
       headers: {
-        "X-Mashape-Key" : key.RECIPE_KEY,
+        "X-Mashape-Key" : API_KEY.SPOONACULAR,
         "Accept" : "application/json"
       }
     }
@@ -34,18 +47,15 @@ module.exports = {
             console.log('This is API response', info);
             cb(info);
           }
-
     }
+    
     request(options, callback);
-
   },
-
-
   getRecipeDetailWithID : function (recipeID, cb) {
     var options = {
       url : RECDETAIL_QUERY+recipeID+"/information?includeNutrition=false",
       headers: {
-        "X-Mashape-Key" : key.RECIPE_KEY,
+        "X-Mashape-Key" : API_KEY.SPOONACULAR,
       }
     }
 
@@ -59,31 +69,107 @@ module.exports = {
 
     request(options, callback);
   },
-
   getFoodType : function (food, cb) {
     console.log('API call is firing');
     var options = {
-      url : FOODTYPE,
+      method: 'POST',
       headers: {
-        "X-Mashape-Key" : key.RECIPE_KEY,
+        "X-Mashape-Key" : API_KEY.SPOONACULAR,
+        "Content-Type": "application/json",
+        "Accept": "application/json"
       },
-      form : {"ingredientList" : food,
-              "servings": 2}
-    }
+      uri: SPOONACULAR.BASE + SPOONACULAR.CLASSIFY,
+      body: {
+        "title": food,"upc":"","plu_code":""
+      },
+      json: true
+    };
 
-    function callback(error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var info = JSON.parse(body);
-        console.log ("getFoodType Callback: ", info);
-        cb(info);
-      } else {
-        console.log('getFoodType error of : ',error)
-      }
+    var matchFoodGroup = this.matchFoodGroup;
+    
+    request(options)
+      .then(function(response){
+        // console.log('response in getFoodType request of : ',response);
+        if(!response.usdaCode){
+          cb({ name: response.cleanTitle, category: "error" });
+        } else {
+          let usdaCode = response.usdaCode.toString();
+          if(usdaCode.length < 5){
+            usdaCode = ("0"+usdaCode).toString();
+          }
+          var optionsUSDA = {
+            method: 'GET',
+            url: USDA.BASE,
+            qs: {
+              ndbno: usdaCode,
+              type: "f",
+              format: "json",
+              api_key: API_KEY.USDA
+            },
+            json: true
+          };
+          request(optionsUSDA)
+            .then(function(responseUSDA){
+              console.log('responseUSDA in getFoodType request of : ',responseUSDA);
+              var foodGroup = matchFoodGroup(responseUSDA.report.food.fg);
+              console.log('foodGroup in responseUSDA is : ',foodGroup);
+              cb({ name: response.cleanTitle, category: foodGroup });
+            })
+            .catch(function(errUSDA){
+              console.log('err in getFoodType USDA request of : ',errUSDA.message);
+              cb({ name: response.cleanTitle, category: "error" });
+            })
+        }
+      })
+      .catch(function(err){
+        console.log('err in getFoodType request of : ',err.message);
+        cb({ name: response.cleanTitle, category: "error" });
+      })
+  },
+  matchFoodGroup: function(input) {
+    input = input.toString().toLowerCase();
+    if(input.includes('dairy')){
+      return 'Dairy';
+    } else if (input.includes('vegetables')) {
+      return 'Vegetables';
+    } else if (input.includes('fruit')) {
+      return 'Fruit';
+    } else if (input.includes('poultry')) {
+      return 'Poultry';
+    } else if (input.includes('pork')) {
+      return 'Pork';
+    } else if (input.includes('beef')) {
+      return 'Beef';
+    } else if (input.includes('finfish')) {
+      return 'Seafood';
+    } else if (input.includes('cereal')) {
+      return 'Grains';
+    } else if (input.includes('sauces')) {
+      return 'Sauces';
+    } else if (input.includes('legumes')) {
+      return 'Legumes';
+    } else if (input.includes('sweets')) {
+      return 'Sweets';
+    } else if (input.includes('snacks')) {
+      return 'Snacks';
+    } else if (input.includes('oils')) {
+      return 'Oils';
+    } else if (input.includes('baked')) {
+      return 'Grains';
+    } else if (input.includes('beverages')) {
+      return 'Beverages';
+    } else if (input.includes('lamb')) {
+      return 'Lamb';
+    } else if (input.includes('nut')) {
+      return 'Nuts';
+    } else if (input.includes('entrees')) {
+      return 'Sides';
+    } else if (input.includes('sausages')) {
+      return 'Meats';
+    } else {
+      return 'N/A';
     }
-    request(options, callback);
   }
-
-
 //closes the module
 }
 
