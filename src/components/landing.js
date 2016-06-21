d0e55d82c2a350b53618da7d3b8743aec86700ab
Sefton419 <austin.sefton@me.com>
2016-06-20 19:37:12 -0700

    [refactor] removes landing page view because it is not completely built out

--- src/components/landing.js
+++ src/components/landing.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GridList, GridTile } from 'material-ui/GridList';
+// import { GridList, GridTile } from 'material-ui/GridList';
 
 // const styles = {
 // 	root: {
