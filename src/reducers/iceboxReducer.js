959fe59fb8f819fed49ad17c55b72e6144268355
Colin Zarnegar <c.zarnegar@gmail.com>
2016-06-20 19:45:57 -0700

    [refactor] refactors iceboxReducer to use applyFoodGroupIcon function

--- src/reducers/iceboxReducer.js
+++ src/reducers/iceboxReducer.js
@@ -48,22 +48,7 @@ export default function (state = [], action) {
 	let newItems;
 	switch (action.type) {
 	case POPULATE_ICEBOX:
-		newItems = action.payload.map(item => {
-			switch (item.foodGroup.toLowerCase()) {
-			case 'fruit':
-				return ({ ...item, key: v4(), iconPath: '../../assets/fruit.png' });
-			case 'vegetables':
-				return ({ ...item, key: v4(), iconPath: '../../assets/vegetables.png' });
-			case 'dairy':
-				return ({ ...item, key: v4(), iconPath: '../../assets/dairy.png' });
-			case 'meat':
-				return ({ ...item, key: v4(), iconPath: '../../assets/meat.png' });
-			case 'grains':
-				return ({ ...item, key: v4(), iconPath: '../../assets/grains.png' });
-			default:
-				return ({ ...item, key: v4(), iconPath: '../../assets/snacks.png' });
-			}
-		});
+		newItems = action.payload.map(item => ({ ...applyFoodGroupIcon(item), key: v4() }));
 		return [...state, ...newItems];
 	case ADD_ITEMS:
 		newItems = action.payload.map(item => ({ ...applyFoodGroupIcon(item), key: v4() }));
